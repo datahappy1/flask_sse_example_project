9c5343b3fd6eba290b028dd02398f68560dcbaa7
datahappy1 <43505494+datahappy1@users.noreply.github.com>
2019-11-06 23:26:56 +0100

    initial commit

--- flaskr/lib/aws.py
+++ flaskr/lib/aws.py
@@ -1,7 +1,9 @@
-import time
 import boto3
 import random
 import time
+from flaskr.config.settings import EMR_JOB_STATUSES_FINISHED_FAILED, \
+    EMR_JOB_STATUSES_FINISHED_SUCCEEDED, EMR_JOB_STATUSES_NON_FINISHED, EMR_JOB_ID
+from flask import session
 
 class EMR:
     """
@@ -16,9 +18,14 @@ class EMR:
     def trigger_step(self):
         pass
 
+def fetch_step_state(step_id):
+    if step_id not in EMR_JOB_STATUSES_FINISHED_SUCCEEDED or step_id not in EMR_JOB_STATUSES_FINISHED_FAILED:
+        step_id_status = ''
+    return step_id_status
+
 def fetch_step_states(job_id):
-    j1= [{'step1': 'running', 'step2': 'pending', 'step3': 'finished'}]
-    j2= [{'step1': 'pending', 'step2': 'failed', 'step3': 'finished'}]
+    j1= [{'step1': 'running'}, {'step2': 'pending'}, {'step3': 'finished'}]
+    j2= [{'step1': 'pending'}, {'step2': 'failed'}, {'step3': 'finished'}]
 
     if random.randint(0, 2) == 1:
         time.sleep(1)
